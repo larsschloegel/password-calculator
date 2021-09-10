cf1e9804b85cff39f469b9b3b3c7a08dacad562a
Lars Schlögel <lars.schloegel@gmail.com>
2021-09-10 18:22:09 +0200

    Ergänzung Passwortgenerator

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -44,6 +47,26 @@ public class PasswordValidation {
             return hasUpper;
         }
 
+        public static String createOneRandomPassword (int lengthRandomPassword){
+            List<String> password = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "p", "r",
+                    "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "M", "N", "P",
+                    "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+            String onePassword ="";
+            for (int i = 0; i < lengthRandomPassword; i++) {
+                onePassword += password.get(new Random().nextInt(password.size()));
+            }
+            return onePassword;
+        }
+        public static String[] createMultiplePassword(int NumberOfPasswordToGenerate, int lenghtOfRandomPassword){
+
+            String[] multiplePasswords = new String[NumberOfPasswordToGenerate];
+            for(int i =0; i<multiplePasswords.length; i++) {
+                multiplePasswords[i] = createOneRandomPassword(lenghtOfRandomPassword);
+            }
+            return multiplePasswords;
+        }
+
+
     public static boolean checkAllRequirements (String password, int minNumberInPassword, int minLengthInPassword){
             return  checkNumberInPassword(password) &&
                     checkMinNumberInPassword(password,minNumberInPassword) &&
@@ -52,13 +75,12 @@ public class PasswordValidation {
                     checkUpperCaseInPassword(password);
         }
 
-
-
-
     public static void main(String[] args) {
 
-
-
+        System.out.println(Arrays.toString(createMultiplePassword(2, 4)));
+        int minCountOfDigitInPassword = 2;
+        int minLengthOfPassword = 1;
+        //System.out.println(createOnePassword(3));
         //System.out.println(checkNumberInPassword("krishna64", 1));
     }
 }
